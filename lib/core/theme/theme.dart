25a2b8b1c3f75086d9d41a84b9878b18c04ba911
zaynom <ouaziz0626@gmail.com>
2024-11-07 17:11:26 +0100

    Add custom theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,10 +2,12 @@ import 'package:app/core/theme/app_palette.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final lightTheme = ThemeData.light().copyWith(
+  static final lightTheme = ThemeData(
+    primaryColor: AppPalette.primaryColor,
     scaffoldBackgroundColor: AppPalette.backgroundColor,
     appBarTheme: const AppBarTheme(
       backgroundColor: AppPalette.backgroundColor,
     ),
+    fontFamily: 'Cairo',
   );
 }
