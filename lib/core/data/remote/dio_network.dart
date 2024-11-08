5d9a01c0368eeadddb948697dac3fc0c9c7712a4
zaynom <ouaziz0626@gmail.com>
2024-11-08 23:55:05 +0100

    Update the baseUrl by removing trailing slash

--- lib/core/data/remote/dio_network.dart
+++ lib/core/data/remote/dio_network.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioNetworkProvider = Provider<Dio>((ref) {
   final options = BaseOptions(
-    baseUrl: 'https://howiyapress.com/wp-json/wp/v2/',
+    baseUrl: 'https://howiyapress.com/wp-json/wp/v2',
     connectTimeout: const Duration(seconds: 60),
     receiveTimeout: const Duration(seconds: 60),
     sendTimeout: const Duration(seconds: 60),
