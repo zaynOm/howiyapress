1b15cb87dcb5a1e49a6bfacad12f8b3ac36ba293
zaynom <ouaziz0626@gmail.com>
2024-11-08 18:30:36 +0100

    Add paginated post entity

---
+++ lib/features/domain/entities/paginated_posts.dart
@@ -0,0 +1,13 @@
+import 'package:app/features/domain/entities/post.dart';
+
+class PaginatedPosts {
+  final List<Post> posts;
+  final int currentPage;
+  final int totalPages;
+
+  PaginatedPosts({
+    required this.posts,
+    required this.currentPage,
+    required this.totalPages,
+  });
+}
