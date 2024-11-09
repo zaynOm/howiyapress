d41dfcf387a4ecc0462e5f8ba9c9308a51e8eca3
zaynom <ouaziz0626@gmail.com>
2024-11-09 17:26:21 +0100

    Add post page

---
+++ lib/features/post/presentation/pages/post_page.dart
@@ -0,0 +1,46 @@
+import 'package:app/core/common/entities/post.dart';
+import 'package:app/core/utils/unescape.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+
+class PostPage extends StatelessWidget {
+  final Post post;
+  const PostPage({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Column(children: [
+            Image.network(
+              post.imageUrl!,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: Column(
+                children: [
+                  Text(
+                    unescape.convert(post.title),
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  HtmlWidget(
+                    unescape.convert(post.content),
+                    textStyle: const TextStyle(fontSize: 16),
+                    // style: const TextStyle(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
